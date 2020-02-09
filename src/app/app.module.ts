42e2c30217b494a30b351e6dadce2edd464b2a1f
Sabas Espin <sabasespin@hotmail.com>
2020-02-09 18:12:22 -0400

    Cambios en modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,28 @@
+import { P404Component } from './shared/p404/p404.component';
+
 import { APP_ROUTES } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { P404Component } from './shared/p404/p404.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcumsComponent } from './shared/breadcums/breadcums.component';
-import { PagesComponent } from './pages/pages.component';
+
+
 import { RegisterComponent } from './login/register.component';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    P404Component,
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcumsComponent,
-    PagesComponent,
     RegisterComponent
+
   ],
   imports: [
+    PagesModule,
     BrowserModule,
     APP_ROUTES
   ],
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,11 @@
 import { RegisterComponent } from './login/register.component';
-import { PagesComponent } from './pages/pages.component';
-import { P404Component } from './shared/p404/p404.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { ProgressComponent } from './pages/progress/progress.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 import {RouterModule, Routes} from '@angular/router' ;
+import { P404Component } from './shared/p404/p404.component';
 
 const appRoutes: Routes = [
-    { path: '', component: PagesComponent, children: [
-        { path: 'dashboard' , component: DashboardComponent } ,
-        { path: 'progress' , component: ProgressComponent } ,
-        { path: 'grafica1' , component: Grafica1Component },
-        { path: '' , redirectTo: '/dashboard' , pathMatch: 'full' }
-    ]
-} ,
+
     { path: 'login' , component: LoginComponent } ,
     { path: 'login' , component: LoginComponent } ,
     { path: 'register' , component: RegisterComponent } ,
